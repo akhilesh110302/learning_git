e365104d6d373ace1ed20568e23da8be20e3a228
akil011 <a1k2k3i4s5@gmail.com>
2023-05-11 13:52:14 +0530

    mesage of hello change to hello world

--- helo.java
+++ helo.java
@@ -1,7 +1,7 @@
 class helo
 {
     public static void main(String[] args) {
-      int a=10, b=20;
-        System.out.println("Hello Word"+ (a+b));
+      
+        System.out.println("Hello Word");
     }
 }
