8cd5babb6dec666aa68ca522510767c12ec2b402
Akhilesh Singh <89310861+Akil011@users.noreply.github.com>
2023-05-11 14:09:50 +0530

    Update Add.java
    Some Changes

--- Add.java
+++ Add.java
@@ -2,6 +2,6 @@ class Add
 {
     public static void main(String[] args) {
       int a=10, b=20;
-        System.out.println("Hello Word"+ (a+b));
+        System.out.println("Hello Word");
     }
 }
